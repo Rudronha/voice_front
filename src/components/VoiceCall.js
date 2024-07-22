d9e6089494445acae639e2da25334a5716f5425d
Rudronha <rudronha@gmail.com>
2024-07-22 14:24:01 +0530

    add .env

--- src/components/VoiceCall.js
+++ src/components/VoiceCall.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000');
+const socket = io(process.env.REACT_APP_BACKEND_URL);
 
 const VoiceCall = () => {
     const localStreamRef = useRef(null);
